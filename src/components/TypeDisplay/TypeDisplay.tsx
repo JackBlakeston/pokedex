85f65823f5b21f86aed73fda5a20573fe7ce65fa
Jack Blakeston <jackblakeston93@gmail.com>
2022-11-12 18:58:23 +0100

    Implement basic TypeDisplay component

--- src/components/TypeDisplay/TypeDisplay.tsx
+++ src/components/TypeDisplay/TypeDisplay.tsx
@@ -1,9 +1,21 @@
+import classes from './TypeDisplay.module.scss';
+
+import { POKEMON_TYPES, TYPE_COLORS } from 'enums';
+
 interface IProps {
-  type: any;
+  type: POKEMON_TYPES;
 }
 
 const TypeDisplay = ({ type }: IProps) => {
-  return <></>;
+  const typeDisplayStyles = {
+    backgroundColor: TYPE_COLORS[type],
+  };
+
+  return (
+    <div className={classes.mainContainer} style={typeDisplayStyles}>
+      {type}
+    </div>
+  );
 };
 
 export default TypeDisplay;
