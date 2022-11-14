29a9c37e6a7d1da7015935be531c3364bb900c39
Jack Blakeston <jackblakeston93@gmail.com>
2022-11-14 18:57:21 +0100

    Create new carousel algorythm

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -12,7 +12,7 @@ const MainPage = () => {
   const { currentPage, nextPage, previousPage, goToNextPage, goToPreviousPage } = usePokemonIdPages();
 
   const [pageAClassNames, setPageAClassNames] = useState(classes.currentPage);
-  const [pageBClassNames, setPageBClassNames] = useState(classes.nextPage);
+  const [pageBClassNames, setPageBClassNames] = useState(`${classes.nextPage} ${classes.hidden}`);
   const [pageCClassNames, setPageCClassNames] = useState(classes.previousPage);
   const [pageAIdList, setPageAIdList] = useState(currentPage);
   const [pageBIdList, setPageBIdList] = useState(nextPage);
@@ -49,46 +49,38 @@ const MainPage = () => {
     }, SLIDE_ANIMATION_TIME);
   };
 
-  const handleNextPageButtonClick = () => {
-    const currentPageId = document.getElementsByClassName(classes.currentPage)[0].id;
+  const removeHiddenClass = (className: string) => {
+    return className.split(' ')[0];
+  };
 
-    if (currentPageId === 'pageA') {
-      setPageAClassNames(classes.previousPage);
-      setPageBClassNames(classes.currentPage);
-      setPageCClassNames(`${classes.nextPage} ${classes.hidden}`);
+  const addHiddenClassWhereNecessary = (className: string, direction: 'previous' | 'next') => {
+    if (direction === 'next' && className === classes.nextPage) {
+      return `${className} ${classes.hidden}`;
     }
-    if (currentPageId === 'pageB') {
-      setPageAClassNames(`${classes.nextPage} ${classes.hidden}`);
-      setPageBClassNames(classes.previousPage);
-      setPageCClassNames(classes.currentPage);
+    if (direction === 'previous' && className === classes.previousPage) {
+      return `${className} ${classes.hidden}`;
     }
-    if (currentPageId === 'pageC') {
-      setPageAClassNames(classes.currentPage);
-      setPageBClassNames(`${classes.nextPage} ${classes.hidden}`);
-      setPageCClassNames(classes.previousPage);
-    }
+    return className;
+  };
+
+  const handleNextPageButtonClick = () => {
+    const pageAClassNamesAux = addHiddenClassWhereNecessary(removeHiddenClass(pageAClassNames), 'next');
+    const pageBClassNamesAux = addHiddenClassWhereNecessary(removeHiddenClass(pageBClassNames), 'next');
+    const pageCClassNamesAux = addHiddenClassWhereNecessary(removeHiddenClass(pageCClassNames), 'next');
+    setPageAClassNames(pageCClassNamesAux);
+    setPageBClassNames(pageAClassNamesAux);
+    setPageCClassNames(pageBClassNamesAux);
     disableButtonsDuringAnimation();
     goToNextPage();
   };
 
   const handlePreviousPageButtonClick = () => {
-    const currentPageId = document.getElementsByClassName(classes.currentPage)[0].id;
-
-    if (currentPageId === 'pageA') {
-      setPageAClassNames(classes.nextPage);
-      setPageBClassNames(`${classes.previousPage} ${classes.hidden}`);
-      setPageCClassNames(classes.currentPage);
-    }
-    if (currentPageId === 'pageB') {
-      setPageAClassNames(classes.currentPage);
-      setPageBClassNames(classes.nextPage);
-      setPageCClassNames(`${classes.previousPage} ${classes.hidden}`);
-    }
-    if (currentPageId === 'pageC') {
-      setPageAClassNames(`${classes.previousPage} ${classes.hidden}`);
-      setPageBClassNames(classes.currentPage);
-      setPageCClassNames(classes.nextPage);
-    }
+    const pageAClassNamesAux = addHiddenClassWhereNecessary(removeHiddenClass(pageAClassNames), 'previous');
+    const pageBClassNamesAux = addHiddenClassWhereNecessary(removeHiddenClass(pageBClassNames), 'previous');
+    const pageCClassNamesAux = addHiddenClassWhereNecessary(removeHiddenClass(pageCClassNames), 'previous');
+    setPageAClassNames(pageBClassNamesAux);
+    setPageBClassNames(pageCClassNamesAux);
+    setPageCClassNames(pageAClassNamesAux);
     disableButtonsDuringAnimation();
     goToPreviousPage();
   };
