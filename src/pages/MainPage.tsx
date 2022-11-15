8128b9469d30e09ca7b2126f72a6d36ba2535b7f
Jack Blakeston <jackblakeston93@gmail.com>
2022-11-15 02:09:30 +0100

    Add title, decoration and other styles

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,16 @@
 import classes from './MainPage.module.scss';
 
 import { GridCarousel } from 'components/adhoc';
+import { ReactComponent as PokeballBackground } from 'assets/icons/pokeball2.svg';
 
 const MainPage = () => {
   return (
     <div className={classes.mainContainer}>
+      <div className={classes.title}>
+        <span>pokemin</span> the minimalist pokedex
+      </div>
       <GridCarousel />
+      <PokeballBackground className={classes.pokeballBackground} />
     </div>
   );
 };
