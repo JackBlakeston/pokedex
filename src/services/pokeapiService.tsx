04706b471170bb7b0b00822461c14fdc85de23ea
Jack Blakeston <jackblakeston93@gmail.com>
2022-11-15 18:17:50 +0100

    Delete obsolete service and improve styles

--- src/services/pokeapiService.tsx
+++ src/services/pokeapiService.tsx
@@ -4,16 +4,6 @@ import { IPokemon } from 'interfaces';
 
 const POKEAPI_URL = 'https://pokeapi.co/api/v2/';
 
-export const getPaginatedPokemonUrls = async (start: number, end: number): Promise<IPokemon[]> => {
-  const url = `${POKEAPI_URL}pokemon/?offset=${start}&?limit=${end}`;
-
-  const res = await fetch(url);
-  const data = await res.json();
-  const pokemonUrlList = data.results;
-
-  return pokemonUrlList;
-};
-
 export const getPokemonById = async (id: number): Promise<IPokemon> => {
   const url = `${POKEAPI_URL}pokemon/${id}`;
 
